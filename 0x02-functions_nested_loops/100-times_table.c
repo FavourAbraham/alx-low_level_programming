b45f0c12464fcc1fa882fb748ba5039b748e7cf2
FavourAbraham <fa4abraham18@gmail.com>
2023-07-19 12:38:41 -0700

    100-time

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,40 @@
  */
 void print_times_table(int n)
 {
-	int row, column, product;
-
 	if (n < 0 || n > 15)
 		return;
 
+	int row, column, product;
+
 	for (row = 0; row <= n; row++)
 	{
-		for (column = 0; column <= n; column++)
+		_putchar('0');
+
+		for (column = 1; column <= n; column++)
 		{
 			product = row * column;
 
-			if (column != 0)
+			_putchar(',');
+			_putchar(' ');
+
+			if (product < 10)
 			{
-				_putchar(',');
 				_putchar(' ');
-
-				if (product < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (product < 100)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar(product + '0');
 			}
-
-			if (product >= 100)
+			else if (product < 100)
+			{
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
 			{
 				_putchar((product / 100) + '0');
 				_putchar(((product / 10) % 10) + '0');
+				_putchar((product % 10) + '0');
 			}
-			else if (product >= 10)
-			{
-				_putchar((product / 10) + '0');
-			}
-
-			_putchar((product % 10) + '0');
 		}
 
 		_putchar('\n');
