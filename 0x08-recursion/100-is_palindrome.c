995ffe33cdba6b0b5d685b12054e3e2052bd7f95
FavourAbraham <fa4abraham18@gmail.com>
2023-09-28 13:56:53 -0700

    Recursion done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int is_palindrome(char *s)
 		return (1);
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
-/** _strlen_recursion - returns the length of a string overthere
+/** _strlen_recursion - returns the length of a strings
  * @s: string to calculate the length
  *
  * Return: length of the string
