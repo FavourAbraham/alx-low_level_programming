de29ce8b1e6d448f02172ee0c2ded1289d8e9828
FavourAbraham <fa4abraham18@gmail.com>
2023-09-28 14:01:03 -0700

    Recursion done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -30,8 +30,7 @@ int _strlen_recursion(char *s)
  * @i:as iterator
  * @len: length of the string
  *
- * Return: 1 if palindrome, 0 if not
- */
+ * Return: 1 if palindrome, 0 if not*/
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
