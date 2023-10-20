664252aff13d556e8a11a136ef614440312fe6c5
FavourAbraham <fa4abraham18@gmail.com>
2023-10-19 21:50:23 -0700

    Done for the 2nd task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * list_len= counts the number of list
+ * h= Point to the head of linked list
+ * Return= returns the element in linked list*
+ *
+ */
+
+	size_t list_len(const list_t *h)
+	{
+	size_t count = 0;
+	while (h != NULL)    
+	{
+        count++;
+        h = h->next;
+    	}
+	return count;
+	}
